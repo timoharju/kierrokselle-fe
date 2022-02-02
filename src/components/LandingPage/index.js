7cf8edec8dcf2d9de730aae2617c95282bc1145a
timoharju <timoharju17@gmail.com>
2022-02-02 18:19:19 +0200

    Component placeholders

---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react"
+
+import { Container } from '@material-ui/core'
+
+const LandingPage = () => {
+  
+  return ( 
+
+  <Container maxWidth="sm"></Container>
+
+  )
+}
+
+
+export default landingPage
