5bc51a701e5e9d6ed6299961a7d829e7a16e499e
timoharju <timoharju17@gmail.com>
2022-04-05 15:36:51 +0300

    new sessionstorage, redux immutable state invariant middleware

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,4 +1,5 @@
 import { combineReducers, createStore, applyMiddleware, compose } from "@reduxjs/toolkit"
+import { loadState, saveState } from "./sessionStorage"
 import sidebarReducer from "../reducers/sidebarReducer"
 import modalReducer from "../reducers/modalReducer"
 import thunk from "redux-thunk"
@@ -10,6 +11,17 @@ const allReducers = combineReducers({
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const store = createStore(allReducers, composeEnhancer(applyMiddleware(thunk)))
+const middleware = process.env.NODE_ENV !== "production" ?
+  [require("redux-immutable-state-invariant").default(), thunk] :
+  [thunk]
+
+const persistedState = loadState()
+
+const store = createStore(allReducers, persistedState, composeEnhancer(applyMiddleware(thunk, ...middleware)))
+
+store.subscribe(() =>  {
+  console.log(store.getState())
+  saveState(store.getState())
+})
 
 export default store
