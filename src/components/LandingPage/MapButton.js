2b2bcde2368c6707302c6c0d11005602e1c2da3d
timoharju <timoharju17@gmail.com>
2022-04-05 15:45:19 +0300

    text size changed

--- src/components/LandingPage/MapButton.js
+++ src/components/LandingPage/MapButton.js
@@ -8,8 +8,8 @@ const MapButton = () => {
   const isClicked = useSelector(state => state.isClicked)
 
   return (
-    <button onClick={() => dispatch(toggleModal(isClicked))}  to="/map" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-6 rounded-full text-lg">
-      <p className="">Etsi lähimmät radat</p>
+    <button onClick={() => dispatch(toggleModal(isClicked))} to="/map" className="bg-gray-600 hover:bg-blue-700 text-white font-bold py-6 px-6  text-lg">
+      <p className="text-2xl">Etsi lähimmät radat</p>
     </button>
   )
 }
