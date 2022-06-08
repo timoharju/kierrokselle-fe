aa3f50704b335583a53ec46688e60a8b0608fe88
timoharju <timoharju17@gmail.com>
2022-06-08 23:52:10 +0300

    Index file cleanup

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,6 +1,3 @@
-import React from "react"
-import MapButton from "../MapPage/MapButton"
-
 
 const LandingPage = () => {
 }
---
+++ src/components/Map/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import MapComponent from "./MapComponent"
+
+const Map = () => {
+
+  return (
+    <MapComponent></MapComponent>
+  )
+}
+
+
+export default Map
