1271105a8687090fcceab9699b9eff6c752ae4fe
timoharju <timoharju17@gmail.com>
2022-06-09 14:35:02 +0300

    Removed react links from Toggleable sidebar

--- src/components/Nav/ToggleableSidebar.js
+++ src/components/Nav/ToggleableSidebar.js
@@ -1,34 +1,31 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
+import { toggleCourseModal } from "../../actions"
+import { toggleSidebar } from "../../actions/"
 
 const ToggleableSidebar = () => {
+
+  const dispatch = useDispatch()
+  const courseModalIsClosed = useSelector(state => state.courseModalIsClosed)
+
   return (
     <div className="bg-gray-800 text-blue-100 w-56 space-y-6 px-2 z-50 absolute inset-y-0 left-0 transform md:relative md:translate-x-0 transition duration-200 ease-in-out">
-      <Link to="/">
-        <p className="text-2xl font-extrabold">Kierrokselle</p>
-      </Link>
+      <p onClick={() => dispatch(toggleSidebar(true))} className="text-2xl font-extrabold">Kierrokselle</p>
       <nav>
-        <Link to="/map">
-          <li className="block text-3xl py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:underline hover:text-white">
+        <li onClick={() => dispatch(toggleSidebar(true))} className="block text-3xl py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:underline hover:text-white">
         Kartta
-          </li>
-        </Link>
-        <Link to="/">
-          <li className="block text-3xl py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:underline hover:text-white">
+        </li>
+        <li onClick={() => {dispatch(toggleCourseModal(courseModalIsClosed)); dispatch(toggleSidebar(true))}} className="block text-3xl py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:underline hover:text-white">
         Radat
-          </li>
-        </Link>
-        <Link to="/">
-          <li className="block text-3xl py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:underline hover:text-white">
+        </li>
+        <li onClick={() => dispatch(toggleSidebar(true))} className="block text-3xl py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:underline hover:text-white">
         Tietoa
-          </li>
-        </Link>
-        <Link to="/">
-          <li className="block text-3xl py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:underline hover:text-white">
+        </li>
+        <li onClick={() => dispatch(toggleSidebar(true))} className="block text-3xl py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:underline hover:text-white">
         Ota yhteyttä
-          </li>
-        </Link>
+        </li>
       </nav>
+      <footer className="absolute bottom-7 text-sm left-2 w-full">{process.env.NODE_ENV !== "production" ? <p>{process.env.NODE_ENV.toUpperCase()} ENVIRONMENT</p> : <></>}</footer>
       <footer className="absolute bottom-4 text-sm left-2 w-full">© 2022 Kierrokselle.com</footer>
     </div>
   )
