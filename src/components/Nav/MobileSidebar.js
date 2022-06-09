ef861b2b1808dd2a53c7f23e7513b0b1d73fbb6f
timoharju <timoharju17@gmail.com>
2022-06-09 14:34:43 +0300

    Removed React links from MobileSidebar

--- src/components/Nav/MobileSidebar.js
+++ src/components/Nav/MobileSidebar.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
 import { toggleSidebar } from "../../actions/"
 
@@ -8,10 +7,8 @@ const MobileSidebar = () => {
   const dispatch = useDispatch()
   const sidebarIsToggled = useSelector(state => state.sidebarIsToggled)
   return (
-    <div className="bg-gray-800 absolute text-gray-100 z-50 flex w-screen justify-between md:hidden">
-      <Link to="/">
-        <p className="block text-2xl p-4 text-white font-bold">Kierrokselle.com</p>
-      </Link>
+    <div className="bg-gray-800 fixed text-gray-100 z-50 flex w-screen justify-between md:hidden">
+      <p className="block text-2xl p-4 text-white font-bold">Kierrokselle.com</p>
       <button onClick={() => dispatch(toggleSidebar(sidebarIsToggled))} className="mobile-menu-button p-4 focus:outline-none focus:bg-gray-700">
         <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
